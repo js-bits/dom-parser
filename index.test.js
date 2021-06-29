947c9a786c01829c2afae36f8cf26c88b4a03b62
ApoloGH <apolo-gh@users.noreply.github.com>
2021-06-29 19:14:44 -0400

    test: add svg format

--- index.test.js
+++ index.test.js
@@ -14,11 +14,23 @@ const xml = `<?xml version="1.0" encoding="utf-8"?>
 </note>
 `;
 
+const svg = `<svg xmlns="http://www.w3.org/2000/svg" height="100" width="100" viewBox="0 0 100 100">
+  <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
+</svg>
+`;
+
 describe(`DOMParser: ${env}`, () => {
-  test('parse', () => {
+  test('parse xml', () => {
     const doc = parse(xml);
     const headings = doc.querySelectorAll('heading');
     expect(headings.length).toEqual(1);
     expect(headings[0].textContent).toEqual('Reminder');
+  });
+
+  test('parse svg', () => {
+    const doc = parse(svg, 'image/svg+xml');
+    const headings = doc.querySelectorAll('circle');
+    expect(headings.length).toEqual(1);
+    expect(headings[0].getAttribute('stroke')).toEqual('black');
   });
 });
