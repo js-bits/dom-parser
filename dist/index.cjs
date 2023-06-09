a57562d22e6cdeb7fd4feef476f8e26658a45c49
ApoloGH <apolo-gh@users.noreply.github.com>
2023-06-08 21:30:54 -0400

    chore: add type checking

--- dist/index.cjs
+++ dist/index.cjs
@@ -4,6 +4,11 @@ var jsdom = require('jsdom');
 
 const Parser = typeof DOMParser === 'undefined' ? new jsdom.JSDOM().window.DOMParser : DOMParser;
 
+/**
+ * @param {string} str
+ * @param {DOMParserSupportedType} mimeType
+ * @returns {HTMLElement}
+ */
 var index = (str, mimeType = 'text/xml') => {
   const doc = new Parser().parseFromString(str, mimeType);
   return doc.documentElement;
--- index.js
+++ index.js
@@ -2,6 +2,11 @@ import jsdom from 'jsdom';
 
 const Parser = typeof DOMParser === 'undefined' ? new jsdom.JSDOM().window.DOMParser : DOMParser;
 
+/**
+ * @param {string} str
+ * @param {DOMParserSupportedType} mimeType
+ * @returns {HTMLElement}
+ */
 export default (str, mimeType = 'text/xml') => {
   const doc = new Parser().parseFromString(str, mimeType);
   return doc.documentElement;
