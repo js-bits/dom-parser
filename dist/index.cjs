2a5f786725765fabfd31add53e4ee673107ba104
ApoloGH <apolo-gh@users.noreply.github.com>
2023-06-08 20:11:28 -0400

    chore: upgrade husky an rollup

--- dist/index.cjs
+++ dist/index.cjs
@@ -2,11 +2,7 @@
 
 var jsdom = require('jsdom');
 
-function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
-
-var jsdom__default = /*#__PURE__*/_interopDefaultLegacy(jsdom);
-
-const Parser = typeof DOMParser === 'undefined' ? new jsdom__default['default'].JSDOM().window.DOMParser : DOMParser;
+const Parser = typeof DOMParser === 'undefined' ? new jsdom.JSDOM().window.DOMParser : DOMParser;
 
 var index = (str, mimeType = 'text/xml') => {
   const doc = new Parser().parseFromString(str, mimeType);
